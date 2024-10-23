47103526086189697b1086f3afdfbf7748c9d295
patriickbr <patrick9009.9@gmail.com>
2024-10-23 12:04:46 +0200

    Los dos primero ejercicios hechos

--- 01_helloWorld/helloWorld.spec.js
+++ 01_helloWorld/helloWorld.spec.js
@@ -1,4 +1,6 @@
-const helloWorld = require('./helloWorld');
+const helloWorld = 
+
+require('./helloWorld');
 
 describe('Hello World', function() {
   test('says "Hello, World!"', function() {
--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(palabraRepetida, numeroRepeticiones) {
+    let resultado = ''; // Inicializa una variable para almacenar el resultado
 
-};
+    for (let i = 0; i < numeroRepeticiones; i++) { // Cambia <= por <
+        if (numeroRepeticiones > 0) {
+            resultado = resultado + palabraRepetida; // Añade la palabra a resultado
+        }
+    }
+    
+    return resultado; // Devuelve el resultado final
+}
+
+console.log(repeatString("hey", 10));
 
 // Do not edit below this line
 module.exports = repeatString;
